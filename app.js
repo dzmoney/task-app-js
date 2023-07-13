3aed6eab77fa29b0a00d5b6f81153c28ad86441c
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-07-13 19:49:41 -0400

    Create selectors

---
+++ app.js
@@ -0,0 +1,4 @@
+let form = document.getElementById("form");
+let newTaskInput = document.getElementById("new-task-input");
+let tasks = document.getElementById("tasks");
+let newTaskBtn = document.getElementById("new-task-btn");
