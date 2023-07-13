7224cbcd06104ebc7835823c694cdc772366c0e6
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-07-13 19:52:45 -0400

    Add msg to warn user of blank input

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let form = document.getElementById("form");
 let newTaskInput = document.getElementById("new-task-input");
+let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let newTaskBtn = document.getElementById("new-task-btn");
