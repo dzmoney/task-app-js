aba28a82c1b831e848a530a5dc51a90991ecc62e
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-07-13 20:08:41 -0400

    Add form validation
    user cannot submit blank task

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ let newTaskInput = document.getElementById("new-task-input");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let newTaskBtn = document.getElementById("new-task-btn");
+let saveTaskButton = document.getElementById("save-task-btn");
+
+// FORM VALIDATION
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  formValidation();
+});
+
+saveTaskButton.addEventListener("click", (event) => {
+  formValidation();
+});
+
+let formValidation = () => {
+  if (newTaskInput.value === "") {
+    console.log("failure");
+    msg.textContent = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.textContent = "";
+  }
+};
